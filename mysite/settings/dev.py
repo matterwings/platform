cf24e63fd9e57c09e7f1634f17211fd61bb45462
Rohith Muthyala <matterwingsinfo@gmail.com>
2023-04-07 22:30:51 +0530

    my app first commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-!8o(*$wqyo8zuv6w&+s7b#z8!#l!h%pb%l$)2&zyc&hm2pdzrb"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
